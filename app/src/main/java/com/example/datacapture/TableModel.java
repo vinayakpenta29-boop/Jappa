e4079031fedcf19eb445d88703ec0344b4514ca6
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2025-09-19 21:07:14 +0530

    Update TableModel.java

--- app/src/main/java/com/example/datacapture/TableModel.java
+++ app/src/main/java/com/example/datacapture/TableModel.java
@@ -1,16 +1,28 @@
 package com.example.datacapture;
 
-public class TableModel {
-    int no;
-    String salesmanNo, barcode, vrpRate, billNo, date, jappa;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
-    public TableModel(int num, String s, String b, String v, String bill, String d, String j) {
-        no = num;
-        salesmanNo = s;
-        barcode = b;
-        vrpRate = v;
-        billNo = bill;
-        date = d;
-        jappa = j;
+@Entity
+public class TableModel {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public int no;
+    public String salesmanNo;
+    public String barcode;
+    public String vrpRate;
+    public String billNo;
+    public String date;
+    public String jappa;
+
+    public TableModel(int no, String salesmanNo, String barcode, String vrpRate, String billNo, String date, String jappa) {
+        this.no = no;
+        this.salesmanNo = salesmanNo;
+        this.barcode = barcode;
+        this.vrpRate = vrpRate;
+        this.billNo = billNo;
+        this.date = date;
+        this.jappa = jappa;
     }
 }
